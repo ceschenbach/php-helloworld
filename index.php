87f183c436c010d1a563b5fc6ac6da04a87fe337
Watson Studio <noreply@ibm.com>
2020-07-20 13:46:32 +0000

    Export from Watson Studio

--- index.php
+++
@@ -1,8 +0,0 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World-Happy Learning. This is wonderful! This is my first Jenkins PHP200 page using OpenShift Online. Testing a change.</p>'; ?> 
- </body>
-</html>
